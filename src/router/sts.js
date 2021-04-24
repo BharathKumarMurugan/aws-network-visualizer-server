a0497826894c103c3b6804431ccd2746e455802c
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-24 23:59:42 +0530

    Add files via upload

---
+++ src/router/sts.js
@@ -0,0 +1,37 @@
+import express from "express";
+import AWS from "../lib/awsConn";
+import appConfig from "../config/appConfig";
+
+const sts = express.Router();
+
+const client = new AWS.STS({ apiVersion: appConfig._aws_api_version });
+
+const getAccountId = async () => {
+    const params = {};
+    try {
+        let { Account: data } = await client
+            .getCallerIdentity(params)
+            .promise();
+        return data;
+    } catch (err) {
+        return {
+            requestID: err.requestId,
+            statusCode: err.statusCode,
+            message: err.code,
+            time: err.time,
+        };
+    }
+};
+
+sts.get("/", async (req, res) => {
+    try {
+        let data = [];
+        data[0] = await getAccountId();
+        data[1] = appConfig._aws_region.toString();
+        res.status(200).send(data);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+export default sts;
