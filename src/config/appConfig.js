8dd3a010992c5acfc0823de1f5b11cddfdd2c957
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-04-26 22:18:48 +0530

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -3,6 +3,9 @@ const appConfig = {};
 appConfig._aws_profile = "default";
 appConfig._api_port = process.env.API_PORT || 5000;
 appConfig._aws_region = "eu-west-1";
-appConfig._aws_api_version = "2016-11-15";
+appConfig._aws_ec2_api_version = "2016-11-15";
+appConfig._aws_rds_api_version = "2014-10-31";
+appConfig._aws_sts_api_version = "2011-06-15";
+appConfig._aws_elbv2_api_version = "2015-12-01";
 
 export default appConfig;
